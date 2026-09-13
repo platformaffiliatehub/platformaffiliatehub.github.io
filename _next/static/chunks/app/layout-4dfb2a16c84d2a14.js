a7a7ad3acad5d5021da958aeab59052f4e459fd5
platformaffiliatehub <platformaffiliatehub@users.noreply.github.com>
2026-09-13 13:53:27 +0530

    Full site redesign: multi-page site, logo, legal pages

---
+++ _next/static/chunks/app/layout-4dfb2a16c84d2a14.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3566:function(e,n,t){Promise.resolve().then(t.t.bind(t,5646,23)),Promise.resolve().then(t.t.bind(t,3950,23)),Promise.resolve().then(t.t.bind(t,6340,23))},5646:function(){}},function(e){e.O(0,[490,340,950,293,528,744],function(){return e(e.s=3566)}),_N_E=e.O()}]);
